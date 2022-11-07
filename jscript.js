83060df860c79a24afece37a233a6ae609b5f269
nenad95 <cosovicnenad14@gmail.com>
2022-11-07 20:22:15 +0100

    first commit

---
+++ jscript.js
@@ -0,0 +1,261 @@
+function show1(){
+
+    const firstDiv = document.createElement('div');
+    firstDiv.textContent = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+    firstDiv.id='div1';
+
+    const secondDiv = document.createElement('div');
+    secondDiv.textContent = "No more then 2GB. All files in your account must fit your alloted storage space."
+    secondDiv.id='div2';
+
+    const thirdDiv = document.createElement('div');
+    thirdDiv.textContent = "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+    thirdDiv.id='div3';
+
+    const forthDiv = document.createElement('div');
+    forthDiv.textContent = "Yes! Send us a message and we'll process your request no questions asked."
+    forthDiv.id='div4';
+
+    const fifthDiv = document.createElement('div');
+    fifthDiv.textContent = "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+    fifthDiv.id='div5';
+
+    const parent = document.getElementById('first-div');
+    const child = document.getElementById('div1');
+    const parent2 = document.getElementById('second-div');
+    const child2 = document.getElementById('div2');
+    const parent3 = document.getElementById('third-div');
+    const child3 = document.getElementById('div3');
+    const parent4 = document.getElementById('forth-div');
+    const child4 = document.getElementById('div4');
+    const parent5 = document.getElementById('fifth-div');
+    const child5 = document.getElementById('div5');
+
+    if (parent.contains(child) !== true) {
+        parent.appendChild(firstDiv);  
+            if (parent2.contains(child2) === true) {child2.parentNode.removeChild(child2)}
+            if (parent3.contains(child3) === true) {child3.parentNode.removeChild(child3)}
+            if (parent4.contains(child4) === true) {child4.parentNode.removeChild(child4)}
+            if (parent5.contains(child5) === true) {child5.parentNode.removeChild(child5)}
+            document.getElementById("head1").style.color="hsl(237, 12%, 33%)";
+            document.getElementById("head1").style.fontWeight = '700';
+
+            document.getElementById("head4").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head4").style.fontWeight = '400';
+            document.getElementById("head3").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head3").style.fontWeight = '400';
+            document.getElementById("head2").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head2").style.fontWeight = '400';
+            document.getElementById("head5").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head5").style.fontWeight = '400';
+    } 
+}
+
+function show2(){
+    const firstDiv = document.createElement('div');
+    firstDiv.textContent = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+    firstDiv.id='div1';
+
+    const secondDiv = document.createElement('div');
+    secondDiv.textContent = "No more then 2GB. All files in your account must fit your alloted storage space."
+    secondDiv.id='div2';
+
+    const thirdDiv = document.createElement('div');
+    thirdDiv.textContent = "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+    thirdDiv.id='div3';
+
+    const forthDiv = document.createElement('div');
+    forthDiv.textContent = "Yes! Send us a message and we'll process your request no questions asked."
+    forthDiv.id='div4';
+
+    const fifthDiv = document.createElement('div');
+    fifthDiv.textContent = "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+    fifthDiv.id='div5';
+
+    const parent = document.getElementById('first-div');
+    const child = document.getElementById('div1');
+    const parent2 = document.getElementById('second-div');
+    const child2 = document.getElementById('div2');
+    const parent3 = document.getElementById('third-div');
+    const child3 = document.getElementById('div3');
+    const parent4 = document.getElementById('forth-div');
+    const child4 = document.getElementById('div4');
+    const parent5 = document.getElementById('fifth-div');
+    const child5 = document.getElementById('div5');
+    
+    if (parent2.contains(child2) !== true) {
+        parent2.appendChild(secondDiv);  
+            if (parent.contains(child) === true) {child.parentNode.removeChild(child)}
+            if (parent3.contains(child3) === true) {child3.parentNode.removeChild(child3)}
+            if (parent4.contains(child4) === true) {child4.parentNode.removeChild(child4)}
+            if (parent5.contains(child5) === true) {child5.parentNode.removeChild(child5)}
+            document.getElementById("head2").style.color="hsl(237, 12%, 33%)";
+            document.getElementById("head2").style.fontWeight = '700';
+
+            document.getElementById("head4").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head4").style.fontWeight = '400';
+            document.getElementById("head3").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head3").style.fontWeight = '400';
+            document.getElementById("head5").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head5").style.fontWeight = '400';
+            document.getElementById("head1").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head1").style.fontWeight = '400';
+    } 
+}
+
+function show3(){
+    const firstDiv = document.createElement('div');
+    firstDiv.textContent = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+    firstDiv.id='div1';
+
+    const secondDiv = document.createElement('div');
+    secondDiv.textContent = "No more then 2GB. All files in your account must fit your alloted storage space."
+    secondDiv.id='div2';
+
+    const thirdDiv = document.createElement('div');
+    thirdDiv.textContent = "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+    thirdDiv.id='div3';
+
+    const forthDiv = document.createElement('div');
+    forthDiv.textContent = "Yes! Send us a message and we'll process your request no questions asked."
+    forthDiv.id='div4';
+
+    const fifthDiv = document.createElement('div');
+    fifthDiv.textContent = "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+    fifthDiv.id='div5';
+
+    const parent = document.getElementById('first-div');
+    const child = document.getElementById('div1');
+    const parent2 = document.getElementById('second-div');
+    const child2 = document.getElementById('div2');
+    const parent3 = document.getElementById('third-div');
+    const child3 = document.getElementById('div3');
+    const parent4 = document.getElementById('forth-div');
+    const child4 = document.getElementById('div4');
+    const parent5 = document.getElementById('fifth-div');
+    const child5 = document.getElementById('div5');
+    
+    
+    if (parent3.contains(child3) !== true) {
+        parent3.appendChild(thirdDiv);  
+            if (parent.contains(child) === true) {child.parentNode.removeChild(child)}
+            if (parent2.contains(child2) === true) {child2.parentNode.removeChild(child2)}
+            if (parent4.contains(child4) === true) {child4.parentNode.removeChild(child4)}
+            if (parent5.contains(child5) === true) {child5.parentNode.removeChild(child5)}
+            document.getElementById("head3").style.color="hsl(237, 12%, 33%)";
+            document.getElementById("head3").style.fontWeight = '700';
+
+            document.getElementById("head4").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head4").style.fontWeight = '400';
+            document.getElementById("head5").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head5").style.fontWeight = '400';
+            document.getElementById("head2").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head2").style.fontWeight = '400';
+            document.getElementById("head1").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head1").style.fontWeight = '400';
+    } 
+}
+
+function show4(){
+    const firstDiv = document.createElement('div');
+    firstDiv.textContent = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+    firstDiv.id='div1';
+
+    const secondDiv = document.createElement('div');
+    secondDiv.textContent = "No more then 2GB. All files in your account must fit your alloted storage space."
+    secondDiv.id='div2';
+
+    const thirdDiv = document.createElement('div');
+    thirdDiv.textContent = "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+    thirdDiv.id='div3';
+
+    const forthDiv = document.createElement('div');
+    forthDiv.textContent = "Yes! Send us a message and we'll process your request no questions asked."
+    forthDiv.id='div4';
+
+    const fifthDiv = document.createElement('div');
+    fifthDiv.textContent = "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+    fifthDiv.id='div5';
+
+    const parent = document.getElementById('first-div');
+    const child = document.getElementById('div1');
+    const parent2 = document.getElementById('second-div');
+    const child2 = document.getElementById('div2');
+    const parent3 = document.getElementById('third-div');
+    const child3 = document.getElementById('div3');
+    const parent4 = document.getElementById('forth-div');
+    const child4 = document.getElementById('div4');
+    const parent5 = document.getElementById('fifth-div');
+    const child5 = document.getElementById('div5');
+    
+    if (parent4.contains(child4) !== true) {
+        parent4.appendChild(forthDiv);  
+            if (parent.contains(child) === true) {child.parentNode.removeChild(child)}
+            if (parent2.contains(child2) === true) {child2.parentNode.removeChild(child2)}
+            if (parent3.contains(child3) === true) {child3.parentNode.removeChild(child3)}
+            if (parent5.contains(child5) === true) {child5.parentNode.removeChild(child5)}
+            document.getElementById("head4").style.color="hsl(237, 12%, 33%)";
+            document.getElementById("head4").style.fontWeight = '700';
+
+            document.getElementById("head5").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head5").style.fontWeight = '400';
+            document.getElementById("head3").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head3").style.fontWeight = '400';
+            document.getElementById("head2").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head2").style.fontWeight = '400';
+            document.getElementById("head1").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head1").style.fontWeight = '400';
+    } 
+}
+
+function show5(){
+    const firstDiv = document.createElement('div');
+    firstDiv.textContent = "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+    firstDiv.id='div1';
+
+    const secondDiv = document.createElement('div');
+    secondDiv.textContent = "No more then 2GB. All files in your account must fit your alloted storage space."
+    secondDiv.id='div2';
+
+    const thirdDiv = document.createElement('div');
+    thirdDiv.textContent = "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you."
+    thirdDiv.id='div3';
+
+    const forthDiv = document.createElement('div');
+    forthDiv.textContent = "Yes! Send us a message and we'll process your request no questions asked."
+    forthDiv.id='div4';
+
+    const fifthDiv = document.createElement('div');
+    fifthDiv.textContent = "Chat and email support is available 24/7. Phone lines are open during normal business hours."
+    fifthDiv.id='div5';
+
+    const parent = document.getElementById('first-div');
+    const child = document.getElementById('div1');
+    const parent2 = document.getElementById('second-div');
+    const child2 = document.getElementById('div2');
+    const parent3 = document.getElementById('third-div');
+    const child3 = document.getElementById('div3');
+    const parent4 = document.getElementById('forth-div');
+    const child4 = document.getElementById('div4');
+    const parent5 = document.getElementById('fifth-div');
+    const child5 = document.getElementById('div5');
+    
+    if (parent5.contains(child5) !== true) {
+        parent5.appendChild(fifthDiv);  
+            if (parent.contains(child) === true) {child.parentNode.removeChild(child)}
+            if (parent2.contains(child2) === true) {child2.parentNode.removeChild(child2)}
+            if (parent3.contains(child3) === true) {child3.parentNode.removeChild(child3)}
+            if (parent4.contains(child4) === true) {child4.parentNode.removeChild(child4)}
+            document.getElementById("head5").style.color="hsl(237, 12%, 33%)";
+            document.getElementById("head5").style.fontWeight = '700';
+
+            document.getElementById("head4").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head4").style.fontWeight = '400';
+            document.getElementById("head3").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head3").style.fontWeight = '400';
+            document.getElementById("head2").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head2").style.fontWeight = '400';
+            document.getElementById("head1").style.color="hsl(238, 29%, 16%)";
+            document.getElementById("head1").style.fontWeight = '400';
+    } 
+} 
